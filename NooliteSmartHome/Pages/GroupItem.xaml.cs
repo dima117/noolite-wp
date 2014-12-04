f990e773aaef2a2c7bb5859cacee0d8f69cb666c
dima117 <dima117a@gmail.com>
2014-12-05 01:57:21 +0300

    - установка видимости элементов, в зависимости от их типа;

--- NooliteSmartHome/Pages/GroupItem.xaml.cs
+++ NooliteSmartHome/Pages/GroupItem.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
+using NooliteSmartHome.Gateway.Configuration;
 
 namespace NooliteSmartHome.Pages
 {
@@ -8,6 +10,8 @@ namespace NooliteSmartHome.Pages
 		{
 			InitializeComponent();
 		}
+
+		#region channel name
 
 		public static readonly DependencyProperty ChannelNameProperty =
 			DependencyProperty.Register("ChannelName", typeof(string), typeof(GroupItem), new PropertyMetadata(default(string), ChannelNamePropertyChanged));
@@ -26,5 +30,42 @@ namespace NooliteSmartHome.Pages
 			get { return (string)GetValue(ChannelNameProperty); }
 			set { SetValue(ChannelNameProperty, value); }
 		}
+
+		#endregion
+
+		#region channel type
+
+		public static readonly DependencyProperty ChannelTypeProperty =
+			DependencyProperty.Register("ChannelType", typeof(Pr1132ChannelUiType), typeof(GroupItem), new PropertyMetadata(default(Pr1132ChannelUiType), ChannelTypePropertyChanged));
+
+		private static void ChannelTypePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			var value = (Pr1132ChannelUiType)e.NewValue;
+			var myControl = d as GroupItem;
+			if (myControl != null)
+			{
+				myControl.PanelSlider.Visibility = GetVisibility(
+					value,
+					Pr1132ChannelUiType.Dimmer,
+					Pr1132ChannelUiType.LED);
+
+				myControl.PanelLed.Visibility = GetVisibility(
+					value,
+					Pr1132ChannelUiType.LED);
+			}
+		}
+
+		public Pr1132ChannelUiType ChannelType
+		{
+			get { return (Pr1132ChannelUiType)GetValue(ChannelTypeProperty); }
+			set { SetValue(ChannelTypeProperty, value); }
+		}
+
+		private static Visibility GetVisibility(Pr1132ChannelUiType target, params Pr1132ChannelUiType[] types)
+		{
+			return types.Contains(target) ? Visibility.Visible : Visibility.Collapsed;
+		}
+
+		#endregion
 	}
 }
